3fc4cfd2d38fd19537bfef238bf34e960ceb1dd9
Thorben Jensen <jensen.thorben@gmail.com>
2019-02-25 22:02:02 +0100

    reformatting

--- calendar_bot/demo_chatbot.py
+++ calendar_bot/demo_chatbot.py
@@ -7,8 +7,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
     CallbackQueryHandler
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
