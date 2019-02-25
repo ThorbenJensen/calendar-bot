d2b1f808c88114fb27badea61ffb5ac5205279b1
Thorben Jensen <jensen.thorben@gmail.com>
2019-02-25 22:00:00 +0100

    adding alpha demo for telegram bot

---
+++ calendar_bot/demo_chatbot.py
@@ -0,0 +1,78 @@
+"""Simple Bot to reply to Telegram messages."""
+
+import logging
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
+    CallbackQueryHandler
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
+                [InlineKeyboardButton("Option 3", callback_data='3')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Hi!', reply_markup=reply_markup)
+
+
+def button(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(text="Selected option: {}".format(query.data),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text('Start converstation with /start')
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Start the bot."""
+    # read token
+    token = None
+    try:
+        with open('secret/bot_token.txt', 'r') as f:
+            token = f.read()
+    except IOError:
+        print('Error: place token for Telegram bot at secret/bot_token.txt')
+
+    # Create the EventHandler and pass it your bot's token
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
